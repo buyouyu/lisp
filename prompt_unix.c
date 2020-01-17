ad0353d81cb72c63c59dd8800da6d17b0c5b25a4
buyouyu <buyouyu2@outlook.com>
2020-01-17 16:08:05 +0800

    parse

--- prompt_unix.c
+++ prompt_unix.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "mpc.h"
 
 #ifdef _WIN32
 #include <string.h>
@@ -42,10 +43,33 @@ int main(int argc,char **argv){
         //fputs do not add \n in the end,fgets will add \n in the end
         //readline will not add \n in the end
 
+        mpc_parser_t* Number = mpc_new("number");
+        mpc_parser_t* Operator = mpc_new("operator");
+        mpc_parser_t* Expr = mpc_new("expr");
+        mpc_parser_t* Lispy= mpc_new("lispy");
+
+        mpca_lang(MPCA_LANG_DEFAULT,
+        "\
+        number : /-?[0-9]+/;\
+        operator : '+'|'-'|'*'|'/';\
+        expr : <number>|'('<operator><expr>+')';\
+        lispy : /^/<operator><expr>+/$/;\
+        ",
+        Number,Operator,Expr,Lispy);
+
         char * input=readline("lispy> ");
         add_history(input);
 
-        printf("hello you input %s\n",input);
+        mpc_result_t r;
+        if(mpc_parse("<stdin>",input,Lispy,&r)){
+            mpc_ast_print(r.output);
+            mpc_ast_delete(r.output);
+        }else{
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
+
+       // printf("hello you input %s\n",input);
         free(input);
     }
     return 0;
